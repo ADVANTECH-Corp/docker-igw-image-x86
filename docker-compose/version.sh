eff4fec9db082c1f1836531e917fae0a0b7a0f3b
ivan0124 <ivan0124.tw@gmail.com>
2017-02-06 13:40:22 +0800

    add hdd-pmq container to docker-compose

--- docker-compose/version.sh
+++ docker-compose/version.sh
@@ -7,6 +7,7 @@ NODE_RED_TAG=latest
 WSN_SIMULATOR_TAG=latest
 WSN_DEV_TAG=${EIS_VER}
 DUST_LINK_TAG=latest
+HDD_PMQ_TAG=latest
 
 
 echo "MQTT_BUS_TAG=${MQTT_BUS_TAG}" > .env
@@ -15,3 +16,4 @@ echo "NODE_RED_TAG=${NODE_RED_TAG}" >> .env
 echo "WSN_SIMULATOR_TAG=${WSN_SIMULATOR_TAG}" >> .env
 echo "WSN_DEV_TAG=${WSN_DEV_TAG}" >> .env
 echo "DUST_LINK_TAG=${DUST_LINK_TAG}" >> .env
+echo "HDD_PMQ_TAG=${HDD_PMQ_TAG}" >> .env
