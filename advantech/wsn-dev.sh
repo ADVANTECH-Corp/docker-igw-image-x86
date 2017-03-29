fd39096ab11816f4a7c50f1177ffaa842887dba3
ericliang12345 <ericliang12345@gmail.com>
2017-03-29 00:01:01 +0000

    [release_test] EIS release test branch Date            : 2017/03/29 Bug #           : N/A Description     : EIS release test  branch Files           : all installer files Reviewer        : Commit by       : Eric.Liang

--- advantech/wsn-dev.sh
+++ advantech/wsn-dev.sh
@@ -2,28 +2,27 @@
 source ./version.sh
 
 #echo "cmd=$1"
-
-IMAGE_NAME=advigw4x86/wisesnail-lib:${WSN_DEV_TAG}
-CONTAINER_NAME=advigw-wisesnail-dev
+image=advigw4x86/wisesnail-lib:v2.0.3
+container_name=advigw-wisesnail-dev
 
 if [ "$1" == "" ] || [ "$1" == "up" ]; then
-	docker run -it --name $CONTAINER_NAME --network=advantech_advigw-network  -v $PWD:/home/adv/workspace:rw $IMAGE_NAME 
+	docker run --privileged -it --name $container_name --network=advantech_advigw-network -v $PWD:/home/adv/workspace:rw $image
 elif [ "$1" == "bash" ]; then
-	docker start $CONTAINER_NAME
-	docker exec -it $CONTAINER_NAME bash
+	docker start $container_name
+	docker exec -it $container_name bash
 elif [ "$1" == "start" ]; then
-	docker start $CONTAINER_NAME
+	docker start $container_name
 elif [ "$1" == "stop" ]; then
-	docker stop $CONTAINER_NAME
+	docker stop $container_name
 elif [ "$1" == "rm" ] || [ "$1" == "down" ]; then
-	docker stop $CONTAINER_NAME
-	docker rm $CONTAINER_NAME
+	docker stop $container_name
+	docker rm $container_name
 elif [ "$1" == "rmi" ]; then
-	docker stop $CONTAINER_NAME
-	docker rm $CONTAINER_NAME
-	docker rmi $IMAGE_NAME
+	docker stop $container_name
+	docker rm $container_name
+	docker rmi $image
 elif [ "$1" == "pull" ]; then
-        docker pull $IMAGE_NAME
+        docker pull $image
 else
     echo "Unknow command: wsn-dev.sh supports these pull, up, start, bash, stop, rm, down, and rmi "
 fi
--- advantech/wsn-sim.sh
+++ advantech/wsn-sim.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
-source ./version.sh
 
 #echo "cmd=$1"
 
-IMAGE_NAME=advigw4x86/wsn-simulator:${WSN_SIMULATOR_TAG}
-CONTAINER_NAME=advigw-wsn-simulator
+image=advigw4x86/wsn-simulator:v2.0.3
+container_name=advigw-wsn-simulator
 
 if [ "$1" == "" ] || [ "$1" == "up" ]; then
-	docker run -it --name $CONTAINER_NAME --network=advantech_advigw-network $IMAGE_NAME
+	docker run -it --name $container_name --network=advantech_advigw-network $image
 elif [ "$1" == "bash" ]; then
-	docker start $CONTAINER_NAME
-	docker exec -it $CONTAINER_NAME bash
+	docker start $container_name
+	docker exec -it $container_name bash
 elif [ "$1" == "start" ]; then
-	docker start $CONTAINER_NAME
+	docker start $container_name
 elif [ "$1" == "stop" ]; then
-	docker stop $CONTAINER_NAME
+	docker stop $container_name
 elif [ "$1" == "rm" ] || [ "$1" == "down" ]; then
-	docker stop $CONTAINER_NAME
-	docker rm $CONTAINER_NAME
+	docker stop $container_name
+	docker rm $container_name
 elif [ "$1" == "rmi" ]; then
-	docker stop $CONTAINER_NAME
-	docker rm $CONTAINER_NAME
-	docker rmi $IMAGE_NAME
+	docker stop $container_name
+	docker rm $container_name
+	docker rmi $image
 elif [ "$1" == "pull" ]; then
-        docker pull $IMAGE_NAME
+        docker pull $image
 else
     echo "Unknow command: Script only supports pull, up, start, bash, stop, rm, down, and rmi!"
 fi
